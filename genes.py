9dd48c2fd61016128b4d5ffb1d247e1a0c38c322
Watson Chao <watsonchao@jubo.health>
2022-12-28 11:56:59 +0800

    cal pos offspring: Codom Dom

--- genes.py
+++ genes.py
@@ -1,3 +1,6 @@
+import pandas as pd
+from itertools import combinations
+
 class Gene:
     pass
 
@@ -27,11 +30,19 @@ class Genes:
         self.gravel = CoDom('YB', 'gravel')
         self.asphalt = CoDom('YB', 'asphalt')
         
-        self.orangeDream = CoDom(None, 'orange dream')
+        self.butter = CoDom('BEL', 'butter')
+
+        self.fire = CoDom('FR', 'fire')
+
+        self.orangeDream = CoDom('OD', 'orange dream')
         
         self.spider = Dom('SP', 'spider')
         
-        self.desertGhost = Res(None, 'desert ghost') 
+        self.leopard = Dom('LP', 'leopard')
+
+        self.banana = CoDom('BN', 'banana')
+
+        self.desertGhost = Res('DG', 'desert ghost') 
 
 
 class Snake:
@@ -41,6 +52,7 @@ class Snake:
             raise Exception(f'Unknown Gender: {gender}')
         self.gender = gender
         self.genes = []
+        self.genes_name = []
         self.super = []
         self.pos_super = []
         for gene in genes:
@@ -51,12 +63,9 @@ class Snake:
             if gene.complex in self.pos_super:
                 self.super.append(gene.complex)
             self.pos_super.append(gene.complex)
-            self.genes.append(gene.name)
-        if len(self.super) is 0: 
-            self.genes_to_provide = len(self.genes)*2
-        else:
-            self.genes_to_provide = (len(self.genes)-len(self.super))*2
-        print(f'Snake Created\n    Gender: {self.gender}\n    Genes: {", ".join(self.genes)}\n')
+            self.genes_name.append(gene.name)
+            self.genes.append(gene)
+        print(f'Snake Created\n    Gender: {self.gender}\n    Genes: {", ".join(self.genes_name)}\n')
 
 
 class Pairing:
@@ -68,11 +77,44 @@ class Pairing:
         self.male = male
         self.female = female
         
-        print(f'Paring created\n    Male: {", ".join(male.genes)}\n    Female: {", ".join(female.genes)}\n')
+        print(f'Paring created\n    Male: {", ".join(male.genes_name)}\n    Female: {", ".join(female.genes_name)}\n')
+        self.male_genes = male.genes_name
+        self.female_genes = female.genes_name
+        self.female_super = []
+        self.male_super = []
+        self.process_super()
+        self.pos_offspring = self.get_pos_offspring()
 
+    def get_pos_offspring(self):
+        output = sum([list(map(list, combinations(self.male_genes+self.female_genes, i))) for i in range(len(self.male_genes+self.female_genes) + 1)], [])
+        if not self.female_super is []:
+            for gene in self.female_super:
+                for l in output:
+                    l.append(gene)
+        if not self.male_super is []:
+            for gene in self.male_super:
+                for l in output:
+                    l.append(gene)
+        return output
+
+    def process_super(self):
+        for gene in self.female.genes:
+            if gene.complex in self.female.super:
+                self.female_super.append(gene.name)
+                self.female_super = list(set(self.female_super))
+                self.female_genes.remove(gene.name) 
+        for gene in self.male.genes:
+            if gene.complex in self.male.super:
+                self.male_super.append(gene.name)
+                self.male_super = list(set(self.male_super))
+                self.male_genes.remove(gene.name) 
+        
 genes = Genes()
 
-Male = Snake('male', genes.asphalt, genes.orangeDream)
-Female = Snake('female', genes.yellowBelly, genes.yellowBelly)
+Male = Snake('male', genes.asphalt, genes.asphalt, genes.banana)
+Female = Snake('female', genes.yellowBelly, genes.orangeDream, genes.fire, genes.leopard)
 
 pair = Pairing(Male, Female)
+pair.get_pos_offspring()
+df = pd.DataFrame(pair.pos_offspring)
+print(f'Possible Offspring: \n{df}')
